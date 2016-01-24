80c681fc2a1e7b0ddcf46a93d4f004a2f27d8c99
Rob Tierolff <teh@borreh.nl>
2016-01-24 15:36:24 +0100

    Accomodated for db changes

--- queries/GetUserResourceActionAccess.sql
+++ queries/GetUserResourceActionAccess.sql
@@ -1,9 +1,9 @@
 SELECT 
 	* 
 FROM 
-	`Group` 
-	JOIN `Grant` ON
-		`Group`.id = `Grant`.group
+	`UserGroup` 
+	JOIN `GroupGrant` ON
+		`UserGroup`.id = `GroupGrant`.`group`
 WHERE 
 	username = @username AND
 	resource = @resource AND
