129db585cef00f15fac56e13390ce3ef6b43276a
Rob Tierolff <teh@borreh.nl>
2016-04-28 22:00:33 +0200

    Engine specific flags

--- data/base.sql
+++ data/base.sql
@@ -17,7 +17,7 @@ CREATE TABLE `GroupGrant` (
   KEY `subresix` (`subresource`),
   KEY `actionix` (`action`),
   KEY `gnix` (`groupname`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+);
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,13 +36,4 @@ CREATE TABLE `UserGroup` (
   PRIMARY KEY (`id`),
   KEY `usernameix` (`username`),
   KEY `nameix` (`groupname`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+);
